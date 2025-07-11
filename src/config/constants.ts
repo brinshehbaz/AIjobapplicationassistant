513a08bd3472e93727ebbeb4a8d93001c0cc50b6
brinshehbaz <braindrain3024@gmail.com>
2025-07-12 04:58:26 +0500

    Fix Token Exchange Error with Correct Redirect URI

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const GOOGLE_CONFIG = {
   CLIENT_ID: '12128787546-72h13fk4lki973f3la4da5nqvvc53lco.apps.googleusercontent.com',
-  REDIRECT_URI: 'https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io/auth/callback',
+  REDIRECT_URI: 'https://localhost:5173/auth/callback',
   SCOPES: [
     'https://www.googleapis.com/auth/userinfo.profile',
     'https://www.googleapis.com/auth/userinfo.email',
